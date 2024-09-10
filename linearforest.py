91a8ce0249bc772bc29f34b7d139671703f30f9a
raaasin <nisarvskp@gmail.com>
2024-09-10 16:51:21 +0530

    change

--- linearforest.py
+++ linearforest.py
@@ -5,6 +5,7 @@ import pandas as pd
 
 class MiniLinearForest:
     def __init__(self, nearest_segments=2, nearest_weight=0.4, rest_weight=0.6):
+        
         self.intercepts =[]
         self.slopes =[]
         self.nearest_weight = nearest_weight
